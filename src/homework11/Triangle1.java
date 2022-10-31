ff8a10ed9085f96d44c0d1eaec62174289015de4
=oxedom <oxedom@gmail.com>
2022-10-31 14:54:00 +0200

    trinagle init

---
+++ src/homework11/Triangle1.java
@@ -0,0 +1,16 @@
+package homework11;
+
+import java.util.Scanner;
+
+public class Triangle1 {
+
+	public static void main(String[] args) {
+	
+			Scanner scan = new Scanner(System.in);
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			int c = scan.nextInt();
+				
+	}
+
+}
