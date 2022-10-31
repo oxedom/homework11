7bd4e2dccb08ffd7f2596644145fcf9586189b93
=oxedom <oxedom@gmail.com>
2022-10-31 15:55:27 +0200

    triangle madness

---
+++ src/homework11/Triangle2.java
@@ -0,0 +1,46 @@
+package homework11;
+
+import java.util.Scanner;
+
+public class Triangle2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+//		Takes 3 parameters from users
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		int z = scan.nextInt();
+		
+		if(x + y <= z || x + z <= y || y + z <= x) {
+			System.out.println("The numbers: "+x+", "+y+" and "+z+" cannot repersent a triangle");
+		}
+		
+		if(x == y && y == z && x == z) {
+			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an equilateral triangle");
+		}
+		
+		if(x * x+y*y == z*z || z*z+y*y==x*x || x*x+z*z == y*y) {
+			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an right-angle triangle");
+		}
+		
+		if(x != y && x != z || y != x && y!= z ) {
+			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an common triangle");
+		}
+		
+		if(x == y || y == z || z == x ) {
+			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an isosceles triangle");
+		}
+		
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
