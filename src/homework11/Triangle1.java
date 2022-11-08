b930f104bcffa0d3afdc6d3c44d6c9ffe90fd9d6
SamLinux <oxedom@gmail.com>
2022-11-08 12:25:55 +0200

    added comments to Homework

--- src/homework11/Triangle1.java
+++ src/homework11/Triangle1.java
@@ -6,12 +6,12 @@ public class Triangle1 {
 
 	public static void main(String[] args) {
 	
+//			scanner import for Input output
 			Scanner scan = new Scanner(System.in);
 			System.out.println ("This program calculates the area "
 					 + "and the perimeter of a given triangle. ");
 					 System.out.println ("Please enter the three lengths"
 					 + " of the triangle's sides");	
-			
 		
 //			Takes 3 parameters from users
 			int a = scan.nextInt();
@@ -21,10 +21,12 @@ public class Triangle1 {
 //			declaring perimeter by adding all three parameters together
 			int perimeter = (a+b+c);
 			
+			//S is a paramater for calculating the area with Heron's formula
+
 			int s = (perimeter/2);
+			// Area calculation with herons forumla
 			double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
 			
-
 			System.out.println("The lengths of the triangle sides are: "+a+","+b+","+c+".");
 			System.out.println("The perimeter of the triangle is: "+perimeter);
 			System.out.println("The area of the triangle is: "+area);
--- src/homework11/Triangle2.java
+++ src/homework11/Triangle2.java
@@ -13,36 +13,26 @@ public class Triangle2 {
 		int y = scan.nextInt();
 		int z = scan.nextInt();
 	
-
+		//Formla to check if numbers can repersent a triangle 
 		if(x + y <= z || x + z <= y || y + z <= x ) {
 			System.out.println("The numbers: "+x+", "+y+" and "+z+" cannot repersent a triangle");
-			
 		}
+		//Formla to check if the triangle is a equilateral 
 		else if (x == y && y == z && x == z) {
 			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an equilateral triangle");
 		}
-	
+		//Formla to check if the triangle is a right angle triangle 
 		else if(Math.pow(x, 2)+ Math.pow(y, 2) == Math.pow(z, 2) || Math.pow(z, 2)+Math.pow(y, 2)==Math.pow(y, 2) || Math.pow(y, 2)+Math.pow(z, 2) == Math.pow(y, 2) ) {
 			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an right-angle triangle");
 		}
-		
+		//Formla to check if the triangle is a common triangle 
 		else if(x != y && x != z || y != x && y!= z ) {
 			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an common triangle");
 		}
-		
+		//Formla to check if the triangle is a isosceles triangle 
 		else if(x == y || y == z || z == x ) {
 			System.out.println("The numbers: "+x+", "+y+" and "+z+" represent an isosceles triangle");
 		}
-
-		
-	
-		
-		
-		
-		
-		
-		
-		
 	}
 
 }
